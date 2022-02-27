f8b69ad1b09a3c6bc15a496ab0f9775984eebf1c
sanaevvv <sanaevvv@gmail.com>
2022-02-27 20:42:10 +0900

    firebase認証追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import styles from './App.module.scss';
 import { Header } from './components/header/Header';
 import { TaskForm } from './features/task/taskForm/TaskForm';
 import { TaskList } from './features/task/taskList/TaskList';
+import { auth } from './firebase';
+// import { UserAuth } from './pages/userAuth/UserAuth';
 
 export const App: React.VFC = () => {
+  console.log(auth);
   return (
     <>
       <div className={styles.wrapper}>
---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, db, provider };
